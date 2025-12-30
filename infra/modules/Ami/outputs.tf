18a2939585289634aec52331079a3c2ba1dbd3d5
parirajan <parirajan@yahoo.com>
2025-12-30 08:59:25 -0800

    Update outputs.tf

--- infra/modules/Ami/outputs.tf
+++ infra/modules/Ami/outputs.tf
@@ -1,11 +1,9 @@
-output "ami_id" {
-  value = aws_ami_from_instance.built.id
+output "ami_id_east" {
+  value       = aws_ami_from_instance.nginx_ami_east.id
+  description = "NGINX AMI ID in us-east-1"
 }
 
-output "builder_instance_id" {
-  value = aws_instance.builder.id
-}
-
-output "base_ami_id" {
-  value = aws_instance.builder.ami
+output "ami_id_west" {
+  value       = aws_ami_copy.nginx_ami_west.id
+  description = "NGINX AMI ID in us-west-2"
 }
