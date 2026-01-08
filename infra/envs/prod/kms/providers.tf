edc6189c2b30b93db786fb0260f0d4e0d5d8027c
parirajan <parirajan@yahoo.com>
2026-01-07 19:44:37 -0800

    Create providers.tf

---
+++ infra/envs/prod/kms/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "pjtfstatebackend"
+    key    = "rtgs/kms-logs/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
