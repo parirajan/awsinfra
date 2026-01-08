26b7e5c9385af664c978ef337ebcf4cfc151245d
parirajan <parirajan@yahoo.com>
2026-01-08 13:33:17 -0800

    Update outputs.tf

--- infra/envs/prod/image/outputs.tf
+++ infra/envs/prod/image/outputs.tf
@@ -1,9 +1,4 @@
 output "image_builder_pipeline_arn" {
-  description = "Image Builder pipeline ARN for prod."
   value       = module.image_builder.pipeline_arn
-}
-
-output "image_builder_recipe_arn" {
-  description = "Image Builder recipe ARN for prod."
-  value       = module.image_builder.image_recipe_arn
+  description = "Image Builder pipeline ARN for rtgs."
 }
