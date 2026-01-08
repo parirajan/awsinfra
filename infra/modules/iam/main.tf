3fbd1a54e8af71c04b951bfc7e9a05291a5513df
parirajan <parirajan@yahoo.com>
2026-01-08 14:11:13 -0800

    Create main.tf

---
+++ infra/modules/iam/main.tf
@@ -0,0 +1,32 @@
+resource "aws_iam_role" "instance" {
+  name = var.role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = var.tags
+}
+
+# AWS‑managed policies recommended for Image Builder EC2 instances
+resource "aws_iam_role_policy_attachment" "imagebuilder_core" {
+  role       = aws_iam_role.instance.name
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilder"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.instance.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "instance" {
+  name = var.instance_profile_name
+  role = aws_iam_role.instance.name
+}
