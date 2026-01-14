0f6cdeb6aebb509d3ba4b6a4c029183ec7df9d1f
parirajan <parirajan@yahoo.com>
2026-01-14 09:10:39 -0800

    Create main.tf

---
+++ infra/modules/ec2-ssm/main.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "this" {
+  name = "${var.name}-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action   = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "this" {
+  name = "${var.name}-ssm-profile"
+  role = aws_iam_role.this.name
+}
+
+data "aws_ssm_parameter" "al2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+resource "aws_instance" "this" {
+  ami                    = data.aws_ssm_parameter.al2023.value
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  iam_instance_profile   = aws_iam_instance_profile.this.name
+
+  tags = merge(
+    { Name = var.name },
+    var.tags
+  )
+}
