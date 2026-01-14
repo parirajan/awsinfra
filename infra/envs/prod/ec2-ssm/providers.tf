288f9225d766e61457c22476daaf44b0f3659af6
parirajan <parirajan@yahoo.com>
2026-01-14 09:16:02 -0800

    Update providers.tf

--- infra/envs/prod/ec2-ssm/providers.tf
+++ infra/envs/prod/ec2-ssm/providers.tf
@@ -1,3 +1,27 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "pjtfstatebackend"      # your TF state bucket
+    key            = "rtgs/ec2-ssm/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "pjtfstatebackend-lock" # optional, if you use state locking
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+
 data "terraform_remote_state" "network" {
   backend = "s3"
   config = {
