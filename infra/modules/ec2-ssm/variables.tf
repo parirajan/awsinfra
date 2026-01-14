ed08b2a162522a6b8e521e8ab1592186c9ee3ca4
parirajan <parirajan@yahoo.com>
2026-01-14 09:11:07 -0800

    Create variables.tf

---
+++ infra/modules/ec2-ssm/variables.tf
@@ -0,0 +1,21 @@
+variable "name" {
+  type = string
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "security_group_id" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
