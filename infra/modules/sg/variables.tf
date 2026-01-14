fced87ce64893fc97680bbf7fa98db9c1d788ba1
parirajan <parirajan@yahoo.com>
2026-01-14 10:14:47 -0800

    Create variables.tf

---
+++ infra/modules/sg/variables.tf
@@ -0,0 +1,10 @@
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
+}
