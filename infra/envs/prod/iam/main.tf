64dc7f48969d0a43ba93b11015bf1fe89b1f7611
parirajan <parirajan@yahoo.com>
2026-01-14 20:35:47 -0800

    Update main.tf

--- infra/envs/prod/iam/main.tf
+++ infra/envs/prod/iam/main.tf
@@ -1,36 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  backend "s3" {
-    bucket         = "pjtfstatebackend"
-    key            = "rtgs/iam-ec2-imagebuilder-ssm/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "pjtfstatebackend-lock"
-    encrypt        = true
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 locals {
   ec2_assume_role = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect = "Allow"
+      Effect    = "Allow"
       Principal = { Service = "ec2.amazonaws.com" }
-      Action   = "sts:AssumeRole"
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-module "ec2_imagebuilder_ssm_iam" {
+module "ec2_ssm_iam" {
   source = "../modules/iam"
 
   role_name               = var.role_name
@@ -42,12 +21,4 @@ module "ec2_imagebuilder_ssm_iam" {
   managed_policy_arns = var.managed_policy_arns
   inline_policies     = var.inline_policies
   tags                = var.tags
-}
-
-output "instance_profile_name" {
-  value = module.ec2_imagebuilder_ssm_iam.instance_profile_name
-}
-
-output "role_name" {
-  value = module.ec2_imagebuilder_ssm_iam.role_name
 }
