7b0095e2e4e54c13eb68772b987932bc01c23c69
parirajan <parirajan@yahoo.com>
2026-01-14 20:32:34 -0800

    Update outputs.tf

--- infra/modules/iam/outputs.tf
+++ infra/modules/iam/outputs.tf
@@ -1,9 +1,19 @@
-output "instance_profile_name" {
-  description = "Name of the instance profile used by Image Builder instances."
-  value       = aws_iam_instance_profile.instance.name
+output "role_name" {
+  description = "Name of the IAM role"
+  value       = aws_iam_role.this.name
 }
 
-output "role_name" {
-  description = "Name of the IAM role used by Image Builder instances."
-  value       = aws_iam_role.instance.name
+output "role_arn" {
+  description = "ARN of the IAM role"
+  value       = aws_iam_role.this.arn
+}
+
+output "instance_profile_name" {
+  description = "Name of the instance profile, if created"
+  value       = try(aws_iam_instance_profile.this[0].name, null)
+}
+
+output "instance_profile_arn" {
+  description = "ARN of the instance profile, if created"
+  value       = try(aws_iam_instance_profile.this[0].arn, null)
 }
