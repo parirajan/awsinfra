f4fc44ffbe5211e1214d6a2059f0a6f853f1ad41
parirajan <parirajan@yahoo.com>
2026-01-14 19:41:37 -0800

    Update variables.tf

--- infra/envs/prod/iam/variables.tf
+++ infra/envs/prod/iam/variables.tf
@@ -1,8 +1,26 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "role_name" {
+  type = string
+}
+
+variable "instance_profile_name" {
+  type = string
+}
+
+variable "managed_policy_arns" {
+  type = list(string)
+}
+
+variable "inline_policies" {
+  type    = map(string)
+  default = {}
+}
+
 variable "tags" {
-  type        = map(string)
-  description = "Common tags for IAM resources in rtgs."
-  default = {
-    Environment = "rtgs"
-    Owner       = "platform"
-  }
+  type    = map(string)
+  default = {}
 }
