7e1500d502792caff529b8faaf75cc7fae9a6282
parirajan <parirajan@yahoo.com>
2026-01-14 20:34:49 -0800

    Update variables.tf

--- infra/envs/prod/iam/variables.tf
+++ infra/envs/prod/iam/variables.tf
@@ -1,26 +1,32 @@
 variable "region" {
-  type    = string
-  default = "us-east-1"
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "role_name" {
-  type = string
+  description = "Name of the IAM role for EC2/SSM"
+  type        = string
 }
 
 variable "instance_profile_name" {
-  type = string
+  description = "Instance profile name for EC2/SSM"
+  type        = string
 }
 
 variable "managed_policy_arns" {
-  type = list(string)
+  description = "List of managed policy ARNs to attach to the role"
+  type        = list(string)
 }
 
 variable "inline_policies" {
-  type    = map(string)
-  default = {}
+  description = "Optional inline policies (name => JSON)"
+  type        = map(string)
+  default     = {}
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  description = "Tags to apply to IAM resources"
+  type        = map(string)
+  default     = {}
 }
