78f970fa6bbe015e5b09c1d5fff69c5ea01bb99f
parirajan <parirajan@yahoo.com>
2026-01-14 19:01:04 -0800

    Update variables.tf

--- infra/envs/prod/sg/variables.tf
+++ infra/envs/prod/sg/variables.tf
@@ -3,6 +3,31 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "name" {
+  type    = string
+  default = "rtgs-utility-ec2-ssm-sg"
+}
+
+variable "ingress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
+}
+
+variable "egress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
+}
+
 variable "tags" {
   type    = map(string)
   default = {}
