231d8dd0b61f2aa271f8e8101f2ee587c906f057
parirajan <parirajan@yahoo.com>
2026-01-14 18:55:51 -0800

    Update variables.tf

--- infra/modules/sg/variables.tf
+++ infra/modules/sg/variables.tf
@@ -1,5 +1,7 @@
+variable "name"   { type = string }
+variable "vpc_id" { type = string }
+
 variable "ingress_rules" {
-  description = "List of ingress rules"
   type = list(object({
     from_port   = number
     to_port     = number
@@ -7,4 +9,19 @@ variable "ingress_rules" {
     cidr_blocks = list(string)
   }))
   default = []
+}
+
+variable "egress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
 }
