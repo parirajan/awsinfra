5bf47d602d04f5767506427ba1a19017e50717b5
parirajan <parirajan@yahoo.com>
2026-01-14 19:01:35 -0800

    Update main.tf

--- infra/envs/prod/sg/main.tf
+++ infra/envs/prod/sg/main.tf
@@ -10,21 +10,9 @@ data "terraform_remote_state" "network" {
 module "ssm_sg" {
   source = "../modules/sg"
 
-  name   = "rtgs-utility-ec2-ssm-sg"
-  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
-
-  # No ingress rules for pure Session Manager access
-  ingress_rules = []
-
-  # Dynamic egress rules: here only HTTPS out
-  egress_rules = [
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-
-  tags = var.tags
+  name          = var.name
+  vpc_id        = data.terraform_remote_state.network.outputs.vpc_id
+  ingress_rules = var.ingress_rules
+  egress_rules  = var.egress_rules
+  tags          = var.tags
 }
