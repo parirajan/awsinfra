eba696d3df5751cb1995da7a4dcaf8e73d2cfabe
parirajan <parirajan@yahoo.com>
2026-01-22 19:35:14 -0800

    Update main.tf

--- svc/main.tf
+++ svc/main.tf
@@ -1,24 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "poccloudbeta1"
+    key    = "anchorbank/network/terraform.tfstate"
+    region = var.region
+  }
+}
+
 data "terraform_remote_state" "ec2_ssm" {
   backend = "s3"
   config = {
-    bucket = "your-state-bucket"
+    bucket = "poccloudbeta1"
     key    = "anchorbank/ec2-ssm/terraform.tfstate"
-    region = "us-east-1"
+    region = var.region
+  }
+}
+
+data "terraform_remote_state" "dns" {
+  backend = "s3"
+  config = {
+    bucket = "poccloudbeta1"
+    key    = "anchorbank/dns/terraform.tfstate"
+    region = var.region
   }
 }
 
 module "nlb_utility" {
   source = "../../../modules/nlb-ec2-ssm"
 
-  name        = "anchorbank-utility"
-  vpc_id      = data.terraform_remote_state.network.outputs.vpc_id
-  subnet_ids  = data.terraform_remote_state.network.outputs.primary_private_subnet_ids
+  name   = "anchorbank-utility"
+  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
 
-  listener_port = 22
-  target_port   = 22
+  public_subnet_ids = data.terraform_remote_state.network.outputs.public_subnet_ids
+
+  listener_port = var.listener_port
+  target_port   = var.target_port
 
   instance_ids = data.terraform_remote_state.ec2_ssm.outputs.instance_ids
 
-  route53_zone_id = data.terraform_remote_state.dns.outputs.private_zone_id
-  dns_name        = "utility.anchorbank.internal"
+  route53_zone_id      = data.terraform_remote_state.dns.outputs.public_zone_id
+  dns_name             = var.dns_name
+  allowed_client_cidrs = var.allowed_client_cidrs
+  tags                 = var.tags
 }
