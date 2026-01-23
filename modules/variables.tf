dfecf347328518a2b13018396f8e272678f18bfb
parirajan <parirajan@yahoo.com>
2026-01-22 19:31:52 -0800

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,35 +1,51 @@
 variable "name" {
-  type = string
+  description = "Base name for NLB resources"
+  type        = string
 }
 
 variable "vpc_id" {
   type = string
 }
 
-variable "subnet_ids" {
-  type = list(string)
+variable "public_subnet_ids" {
+  description = "Public subnet IDs where NLB ENIs live"
+  type        = list(string)
 }
 
 variable "listener_port" {
-  type    = number
-  default = 22  # or 443 / custom
+  description = "Port exposed on the NLB"
+  type        = number
+  default     = 22
 }
 
 variable "target_port" {
-  type    = number
-  default = 22
+  description = "Port on EC2 instances"
+  type        = number
+  default     = 22
 }
 
 variable "instance_ids" {
-  type = list(string)
+  description = "EC2 instance IDs (targets in private subnets)"
+  type        = list(string)
 }
 
 variable "route53_zone_id" {
-  description = "Hosted zone ID for DNS record"
+  description = "Hosted zone ID for NLB DNS record"
   type        = string
 }
 
 variable "dns_name" {
-  description = "DNS name to create (e.g. utility.anchorbank.internal)"
+  description = "Record name (e.g. utility.anchorbank.com)"
   type        = string
+}
+
+variable "allowed_client_cidrs" {
+  description = "CIDRs allowed to hit the NLB"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
 }
