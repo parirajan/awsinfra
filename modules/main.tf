d331bf7bef910bebf84a656a16abdf33e5c96e61
parirajan <parirajan@yahoo.com>
2026-01-22 19:33:30 -0800

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,24 +1,59 @@
+resource "aws_security_group" "nlb" {
+  name        = "${var.name}-nlb-sg"
+  description = "SG for public NLB"
+  vpc_id      = var.vpc_id
+
+  # Inbound from clients on listener port
+  ingress {
+    from_port   = var.listener_port
+    to_port     = var.listener_port
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_client_cidrs
+  }
+
+  # Outbound to instances (allow all, stateful)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.tags, {
+    Name = "${var.name}-nlb-sg"
+  })
+}
+
 resource "aws_lb" "this" {
   name               = "${var.name}-nlb"
   load_balancer_type = "network"
-  subnets            = var.subnet_ids
-  internal           = true
+  internal           = false
+  subnets            = var.public_subnet_ids
+
+  # NLB SG support requires recent AWS provider
+  security_groups = [aws_security_group.nlb.id]
 
   enable_cross_zone_load_balancing = true
+
+  tags = merge(var.tags, {
+    Name = "${var.name}-nlb"
+  })
 }
 
 resource "aws_lb_target_group" "this" {
-  name     = "${var.name}-tg"
-  port     = var.target_port
-  protocol = "TCP"
-  vpc_id   = var.vpc_id
-
+  name        = "${var.name}-tg"
+  port        = var.target_port
+  protocol    = "TCP"
+  vpc_id      = var.vpc_id
   target_type = "instance"
+
+  tags = merge(var.tags, {
+    Name = "${var.name}-tg"
+  })
 }
 
 resource "aws_lb_target_group_attachment" "this" {
-  for_each = toset(var.instance_ids)
-
+  for_each         = toset(var.instance_ids)
   target_group_arn = aws_lb_target_group.this.arn
   target_id        = each.value
   port             = var.target_port
