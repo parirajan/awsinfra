7e78751d9bfd7e6100546ef412ff8ea9e4e1b86b
parirajan <parirajan@yahoo.com>
2026-02-11 18:51:32 -0800

    Create MQConfig.java

---
+++ egress-service/src/main/java/pvt/aotibank/payments/egress/config/MQConfig.java
@@ -0,0 +1,8 @@
+package pvt.aotibank.payments.egress.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MQConfig {
+    // Config logic if needed (e.g. custom JmsListenerContainerFactory)
+}
