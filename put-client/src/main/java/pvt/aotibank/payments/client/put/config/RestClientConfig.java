7a1e65c72582e726b60d367bd18043ae1d1f1dbf
parirajan <parirajan@yahoo.com>
2026-02-12 16:18:32 -0800

    Update RestClientConfig.java

--- put-client/src/main/java/pvt/aotibank/payments/client/put/config/RestClientConfig.java
+++ put-client/src/main/java/pvt/aotibank/payments/client/put/config/RestClientConfig.java
@@ -3,8 +3,6 @@ package pvt.aotibank.payments.client.put.config;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
-import org.apache.hc.client5.http.io.HttpClientConnectionManager;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
 import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +13,7 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
+import java.io.InputStream;
 import java.security.KeyStore;
 
 @Configuration
@@ -22,43 +21,55 @@ public class RestClientConfig {
 
     @Value("${client.ssl.key-store}")
     private Resource keyStore;
+
     @Value("${client.ssl.key-store-password}")
     private String keyStorePassword;
+
     @Value("${client.ssl.trust-store}")
     private Resource trustStore;
+
     @Value("${client.ssl.trust-store-password}")
     private String trustStorePassword;
 
     @Bean
     public RestTemplate mtlsRestTemplate() throws Exception {
-        // 1. Load Keys and Trust Material
+        // 1. Load Client KeyStore from Stream (JAR-safe)
+        KeyStore clientKeyStore = KeyStore.getInstance("PKCS12");
+        try (InputStream is = keyStore.getInputStream()) {
+            clientKeyStore.load(is, keyStorePassword.toCharArray());
+        }
+
+        // 2. Load TrustStore from Stream (JAR-safe)
+        KeyStore trustKeyStore = KeyStore.getInstance("PKCS12");
+        try (InputStream is = trustStore.getInputStream()) {
+            trustKeyStore.load(is, trustStorePassword.toCharArray());
+        }
+
+        // 3. Build SSL Context using KeyStore OBJECTS (not file paths)
         SSLContext sslContext = SSLContextBuilder.create()
-                .loadKeyMaterial(keyStore.getFile(), keyStorePassword.toCharArray(), keyStorePassword.toCharArray())
-                .loadTrustMaterial(trustStore.getFile(), trustStorePassword.toCharArray())
+                .loadKeyMaterial(clientKeyStore, keyStorePassword.toCharArray())
+                .loadTrustMaterial(trustKeyStore, null) // null = trust all in truststore
                 .build();
 
-        // 2. Configure Apache HttpClient with SSL
-        SSLConnectionSocketFactory sslSocketFactory = SSLConnectionSocketFactoryBuilder.create()
-                .setSslContext(sslContext)
-                .build();
-
-        HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
-                .setSSLSocketFactory(sslSocketFactory)
-                .build();
-
+        // 4. Configure HttpClient
         CloseableHttpClient httpClient = HttpClients.custom()
-                .setConnectionManager(cm)
+                .setConnectionManager(PoolingHttpClientConnectionManagerBuilder.create()
+                        .setSSLSocketFactory(SSLConnectionSocketFactoryBuilder.create()
+                                .setSslContext(sslContext)
+                                .build())
+                        .build())
                 .build();
 
-        // 3. Bind to RestTemplate
         return new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
     }
 
-    // Bean for the PayloadSigner to access the private key raw
     @Bean
     public KeyStore signingKeyStore() throws Exception {
         KeyStore ks = KeyStore.getInstance("PKCS12");
-        ks.load(keyStore.getInputStream(), keyStorePassword.toCharArray());
+        // Also fix the signing bean to use InputStream
+        try (InputStream is = keyStore.getInputStream()) {
+            ks.load(is, keyStorePassword.toCharArray());
+        }
         return ks;
     }
 }
